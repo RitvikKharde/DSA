35bbca6d6874ed97b16298a71254ff495edfba77
Ritvik Kharde <kharderitvik9@gmail.com>
2025-03-31 01:22:34 +0530

    Time: 2 ms (77.99%), Space: 7.8 MB (35.53%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fib(int n) {
+        int n1=0;
+        int n2=1;
+        int fn=1;
+        if (n==0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        if (2<=n<=30){
+        for(int i=0;i<=n-2;i++){
+            fn=n1+n2;
+            n1=n2;
+            n2=fn;
+        }
+        }
+    return fn;
+    }
+};
