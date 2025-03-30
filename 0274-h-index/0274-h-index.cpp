1b03e032bb2fcc2caf9e7cdd9f28b8a316a10f8c
Ritvik Kharde <kharderitvik9@gmail.com>
2025-03-31 01:21:43 +0530

    Time: 0 ms (100%), Space: 12.4 MB (41.77%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        sort(citations.begin(), citations.end(), greater<int>());
+        
+        int h = 0; 
+        
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= h + 1) {
+                h++;  
+                 } else {
+                break;  
+                  }
+        }
+        return h;
+    }
+};
