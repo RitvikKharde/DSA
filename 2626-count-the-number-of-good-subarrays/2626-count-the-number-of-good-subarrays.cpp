697112c9557d29d48e29c4cf9297c248b9094edb
Ritvik Kharde <kharderitvik9@gmail.com>
2025-04-16 17:26:42 +0530

    Time: 72 ms (88.63%), Space: 79 MB (77.96%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
-        int n = nums.size();
         unordered_map<int, int> freq;
-        long long res = 0;
+        long long res = 0, pairs = 0;
         int left = 0;
-        long long pairs = 0;
+        int n = nums.size();
 
         for (int right = 0; right < n; ++right) {
             pairs += freq[nums[right]];
             freq[nums[right]]++;
 
             while (pairs >= k) {
-                res += (n - right);
-                pairs -= (freq[nums[left]] - 1);
+                res += (n - right);  
                 freq[nums[left]]--;
+                pairs -= freq[nums[left]];
                 left++;
             }
         }
