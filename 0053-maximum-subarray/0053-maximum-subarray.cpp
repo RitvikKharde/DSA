5a03f0bde2d3a21167b70a4134d5659817898d2e
Ritvik Kharde <kharderitvik9@gmail.com>
2025-04-22 11:23:37 +0530

    Time: 0 ms (100%), Space: 71.8 MB (53.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,15 +3,15 @@ public:
     int maxSubArray(vector<int>& nums) {
         if (nums.empty()) return 0; 
         int currentMax = nums[0];
-        int globalMax = nums[0];
+        int finalMax = nums[0];
 
         for (int i = 1; i < nums.size(); ++i) {
             currentMax = max(nums[i], currentMax + nums[i]);
-            if (currentMax > globalMax) {
-                globalMax = currentMax;
+            if (currentMax > finalMax) {
+                finalMax = currentMax;
             }
         }
 
-        return globalMax;
+        return finalMax;
     }
 };
