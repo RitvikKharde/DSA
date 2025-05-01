18fc5abc0dbb758bf75b90da611094d313f87a4d
Ritvik Kharde <kharderitvik9@gmail.com>
2025-05-01 23:31:27 +0530

    Time: 631 ms (69.38%), Space: 286.5 MB (31.78%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
@@ -1,55 +1,105 @@
+// class Solution {
+// public:
+//     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int p, int strength) {
+//         int n = tasks.size(), m = workers.size();
+        
+//         sort(tasks.begin(), tasks.end());
+//         sort(workers.begin(), workers.end());
+//         int lo = 0, hi = min(m, n);
+//         int ans;
+        
+//         while(lo <= hi) {
+//             int mid = lo + (hi - lo) / 2;
+//             int count = 0;
+//             bool flag = true;
+            
+//             multiset<int> st(workers.begin(), workers.end());
+            
+//             for(int i = mid - 1; i >= 0; i--) {
+                
+//                 auto it = prev(st.end());
+//                 if(tasks[i] <= *it) {
+                    
+//                     st.erase(it);
+//                 } else {
+                    
+//                     auto it = st.lower_bound(tasks[i] - strength);
+//                     if(it != st.end()) {
+                        
+//                         count++;
+//                         st.erase(it);
+//                     } else {
+                        
+//                         flag = false;
+//                         break;
+//                     }
+//                 }
+                
+//                 if(count > p) {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+            
+//             if(flag) {
+//                 ans = mid;
+//                 lo = mid + 1;
+//             } else {
+//                 hi = mid - 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int p, int strength) {
-        int n = tasks.size(), m = workers.size();
-        
-        sort(tasks.begin(), tasks.end());
-        sort(workers.begin(), workers.end());
-        int lo = 0, hi = min(m, n);
-        int ans;
-        
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            int count = 0;
-            bool flag = true;
-            
-            multiset<int> st(workers.begin(), workers.end());
-            
-            for(int i = mid - 1; i >= 0; i--) {
-                
-                auto it = prev(st.end());
-                if(tasks[i] <= *it) {
-                    
-                    st.erase(it);
-                } else {
-                    
-                    auto it = st.lower_bound(tasks[i] - strength);
-                    if(it != st.end()) {
-                        
-                        count++;
-                        st.erase(it);
-                    } else {
-                        
-                        flag = false;
-                        break;
-                    }
-                }
-                
-                if(count > p) {
-                    flag = false;
-                    break;
-                }
-            }
-            
-            if(flag) {
-                ans = mid;
-                lo = mid + 1;
+    bool canPerform(int taskCount, const vector<int>& tasks, const vector<int>& workers, int pills, int boost) {
+        multiset<int> pool(workers.end() - taskCount, workers.end());
+        int usedPills = 0;
+
+        for (int i = taskCount - 1; i >= 0; --i) {
+            int required = tasks[i];
+            auto strongest = pool.end();
+            --strongest;
+
+            if (*strongest >= required) {
+                pool.erase(strongest);
             } else {
-                hi = mid - 1;
+                if (usedPills >= pills) return false;
+
+                auto withPill = pool.lower_bound(required - boost);
+                if (withPill == pool.end()) return false;
+
+                pool.erase(withPill);
+                ++usedPills;
             }
         }
-        return ans;
+
+        return true;
+    }
+
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        sort(tasks.begin(), tasks.end());
+        sort(workers.begin(), workers.end());
+
+        int low = 0, high = min((int)tasks.size(), (int)workers.size());
+        int maxTasks = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canPerform(mid, tasks, workers, pills, strength)) {
+                maxTasks = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return maxTasks;
     }
 };
-
 
