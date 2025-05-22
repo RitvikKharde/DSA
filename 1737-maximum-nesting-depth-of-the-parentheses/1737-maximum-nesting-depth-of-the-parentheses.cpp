b3908b8f71fbe36f4a4e212dab22d4c5c5285f0f
Ritvik Kharde <kharderitvik9@gmail.com>
2025-05-22 18:29:39 +0530

    Time: 0 ms (100%), Space: 8.2 MB (85.18%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,42 @@
+//Brute force
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt = 0;      
+        int maxc = 0;     
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                cnt++;
+                maxc = max(maxc, cnt);  
+            } else if (s[i] == ')') {
+                cnt--;
+            }
+        }
+
+        return maxc;
+    }
+};
+
+
+
+//Using stack
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         stack<char> stk;
+//         int maxDepth = 0;
+
+//         for (char ch : s) {
+//             if (ch == '(') {
+//                 stk.push(ch);
+//                 maxDepth = max(maxDepth, (int)stk.size()); 
+//             } else if (ch == ')') {
+//                 if (!stk.empty()) {
+//                     stk.pop(); 
+//                 }
+//             }
+//         }
+
+//         return maxDepth;
+//     }
+// };
