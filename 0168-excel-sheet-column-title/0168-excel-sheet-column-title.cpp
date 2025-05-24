74c8f10c93c5375f23fc64329d8d7229a4b9d8ce
Ritvik Kharde <kharderitvik9@gmail.com>
2025-05-24 17:47:22 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.08%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,26 +1,47 @@
+//Brute Force
+// class Solution {
+// public:
+//     string convertToTitle(int columnNumber) {
+//         map<char, int> letterMap;
+//         for (char ch = 'A'; ch <= 'Z'; ++ch) {
+//             letterMap[ch] = ch - 'A' + 1;
+//         }
+
+//         string finalLetter = "";
+
+//         if (columnNumber <= 26) {
+//             char letter = 'A' + (columnNumber - 1);
+//             finalLetter += letter;
+//         } else {
+//             while (columnNumber > 0) {
+//                 columnNumber--; 
+//                 int remainder = columnNumber % 26;
+//                 char letter = 'A' + remainder;
+//                 finalLetter = letter + finalLetter;  
+//                 columnNumber /= 26;
+//             }
+//         }
+
+//         return finalLetter;
+//     }
+// };
+
+
+
+//Optimal Solution-
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        map<char, int> letterMap;
-        for (char ch = 'A'; ch <= 'Z'; ++ch) {
-            letterMap[ch] = ch - 'A' + 1;
+        string result = "";
+
+        while (columnNumber > 0) {
+            columnNumber--;   
+            int remainder = columnNumber % 26;
+            char letter = 'A' + remainder;
+            result = letter + result;  
+            columnNumber /= 26;
         }
 
-        string finalLetter = "";
-
-        if (columnNumber <= 26) {
-            char letter = 'A' + (columnNumber - 1);
-            finalLetter += letter;
-        } else {
-            while (columnNumber > 0) {
-                columnNumber--; 
-                int remainder = columnNumber % 26;
-                char letter = 'A' + remainder;
-                finalLetter = letter + finalLetter;  
-                columnNumber /= 26;
-            }
-        }
-
-        return finalLetter;
+        return result;
     }
 };
